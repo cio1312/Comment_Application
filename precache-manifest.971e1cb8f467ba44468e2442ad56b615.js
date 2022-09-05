2421b29e96279dd79b64259b5af57899fb527a7c
Valencio ryan Fernandes <>
2022-09-05 06:22:02 +0530

    Updates

---
+++ precache-manifest.971e1cb8f467ba44468e2442ad56b615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b38fc2bdc6d510e047538344b94c3bb",
+    "url": "/Comment_Application/index.html"
+  },
+  {
+    "revision": "9807b332e48770d35572",
+    "url": "/Comment_Application/static/css/main.ffb6d7f9.chunk.css"
+  },
+  {
+    "revision": "216e2c507fe4517956e0",
+    "url": "/Comment_Application/static/js/2.8fab0177.chunk.js"
+  },
+  {
+    "revision": "9807b332e48770d35572",
+    "url": "/Comment_Application/static/js/main.162cf36d.chunk.js"
+  },
+  {
+    "revision": "9be5ae8061d3f6f1d46d",
+    "url": "/Comment_Application/static/js/runtime~main.093e311c.js"
+  }
+]);
